dabf91a61ecb0bb1b4c7855c63d85fc315106f7b
sezinserficeli <sezinserficeli@std.iyte.edu.tr>
2023-06-03 00:23:36 +0300

    alt-navbar hover change

--- src/common/components/navbar/alt-navbar.js
+++ src/common/components/navbar/alt-navbar.js
@@ -31,20 +31,20 @@ const GenreContent = () => {
 
       <div className="grid grid-cols-2 gap-x-20">
         <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
-        <OptionButton genre text={"Action"} to={"actionId"} />
+        <OptionButton genre text={"Adventure"} to={"adventureId"} />
+        <OptionButton genre text={"Classics"} to={"classicsId"} />
+        <OptionButton genre text={"Contemporary"} to={"contemporaryId"} />
+        <OptionButton genre text={"Fantasy"} to={"fantasyId"} />
+        <OptionButton genre text={"Historical"} to={"historicalId"} />
+        <OptionButton genre text={"Horror"} to={"horrorId"} />
+        <OptionButton genre text={"Mythology"} to={"mythologyId"} />
+        <OptionButton genre text={"Non-fiction"} to={"nonfictionId"} />
+        <OptionButton genre text={"Plays"} to={"playsId"} />
+        <OptionButton genre text={"Poetry"} to={"poetryId"} />
+        <OptionButton genre text={"Psychology"} to={"psychologyId"} />
+        <OptionButton genre text={"Romance"} to={"romanceId"} />
+        <OptionButton genre text={"Sci-Fi"} to={"scifiId"} />
+        <OptionButton genre text={"Thriller"} to={"thrillerId"} />
       </div>
     </div>
   );
@@ -55,7 +55,7 @@ const CityButton = ({ text, to }) => {
     <div className="flex flex-col gap-2">
       <OptionButton
         className={
-          "p-3 text-lg font-bold text-[#565555] text-center  bg-yellow-200 rounded-md"
+          "p-3 text-lg font-bold text-[#565555] text-center hover:bg-yellow-100 hover:shadow-xl bg-yellow-200 rounded-md"
         }
         text={to}
         to={"01"}
@@ -70,18 +70,18 @@ const CityContent = () => {
   return (
     <div className="px-2 font-montserrat">
       <div className="mx-20 mb-6 text-lg text-center underline font-bold text-[#565555]">
-        Pick the city to look books!
+        Pick the city to search books!
       </div>
 
       <div className="grid grid-cols-10 gap-y-5 gap-x-4">
         <CityButton text={"Adana"} to={"01"} />
-        <CityButton text={"Adana"} to={"01"} />
-        <CityButton text={"Adana"} to={"01"} />
-        <CityButton text={"Adana"} to={"01"} />
-        <CityButton text={"Adana"} to={"01"} />
-        <CityButton text={"Adana"} to={"01"} />
-        <CityButton text={"Adana"} to={"01"} />
-        <CityButton text={"Adana"} to={"01"} />
+        <CityButton text={"Ankara"} to={"06"} />
+        <CityButton text={"Antalya"} to={"09"} />
+        <CityButton text={"Istanbul"} to={"34"} />
+        <CityButton text={"Izmir"} to={"35"} />
+        <CityButton text={"Manisa"} to={"45"} />
+        <CityButton text={"Aydın"} to={"01"} />
+        <CityButton text={"Muğla"} to={"01"} />
         <CityButton text={"Adana"} to={"01"} />
         <CityButton text={"Adana"} to={"01"} />
         <CityButton text={"Adana"} to={"01"} />
