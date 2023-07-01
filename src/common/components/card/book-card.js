6e43b70992bed7b61c0997a19592f1b7ba3a9d4f
Berke Udunman <berkeudunman@gmail.com>
2023-07-01 20:09:04 +0300

    book-card-fix

--- src/common/components/card/book-card.js
+++ src/common/components/card/book-card.js
@@ -3,17 +3,10 @@ import sample from "../../../assets/sample.png";
 import React from "react";
 import { useDispatchCart } from "../cart/cartProvider";
 
-const BookCard = ({ book, background }) => {
+const BookCard = ({ book }) => {
   return (
-    <div className="p-2 bg-[#FFFBEB] rounded-md hover:shadow-sm border">
-      <div
-        className="flex justify-center bg-cover bg-fixed text-white font-bold  px-2 "
-        style={{
-          backgroundImage: `url(${background})`,
-        }}
-      >
-        <ItemContent data={book} />
-      </div>
+    <div className="p-2 bg-[#FFFBEB] rounded-md hover:shadow-md border">
+      <ItemContent data={book} />
     </div>
   );
 };
@@ -27,28 +20,26 @@ const ItemContent = ({ data }) => {
     dispatch({ type: "ADD", item });
   };
   return data ? (
-    <div className="text-xs grid gap-y-3 px-2 font-normal">
-      <div className="flex-1 justify-center p-10 w-full">
+    <div className="flex flex-col justify-between text-xs gap-y-3 px-2 font-normal h-full">
+      <div className="p-6">
         <img src={sample} alt="" />
       </div>
 
-      <div className="flex gap-x-3 justify-between">
-        <p className="font-bold text-gray-700">{data.title}</p>
+      <div className="flex flex-wrap flex-col gap-x-3">
+        <p className="font-bold text-gray-700 flex-wrap">{data.title}</p>
         <p className="text-gray-500">{data.author}</p>
       </div>
-      <div className="flex">
-        <div>
-          <p className="text-black">{data.location}</p>
+      <div className="">
+        <p className="text-black">{data.location}</p>
+        <div className="flex gap-8">
           <p className="text-[#3BC3FF] ">{data.seller}</p>
+          <p className="text-black text-base">{data.price}TL</p>
         </div>
-      </div>
-      <div className="self-center">
-        <p className="text-black text-base">{data.price}TL</p>
       </div>
 
       <Button
         onClick={() => addToCart({ data })}
-        className="bg-white border border-black flex-1 w-full"
+        className="bg-white border border-black  "
       >
         Add To Cart
       </Button>
