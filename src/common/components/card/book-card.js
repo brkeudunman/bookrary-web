7f33942acd5b061dc21b862cd0ef59426444594e
Berke Udunman <berkeudunman41@gmail.com>
2023-07-02 15:28:17 +0300

    book-card

--- src/common/components/card/book-card.js
+++ src/common/components/card/book-card.js
@@ -2,10 +2,11 @@ import { Button, Spin } from "antd";
 import sample from "../../../assets/sample.png";
 import React from "react";
 import { useDispatchCart } from "../cart/cartProvider";
+import { Link } from "react-router-dom";
 
 const BookCard = ({ book }) => {
   return (
-    <div className="p-2 bg-[#FFFBEB] rounded-md hover:shadow-md border">
+    <div className="p-2 h-full  bg-[#FFFBEB] rounded-md hover:shadow-md border">
       <ItemContent data={book} />
     </div>
   );
@@ -20,30 +21,32 @@ const ItemContent = ({ data }) => {
     dispatch({ type: "ADD", item });
   };
   return data ? (
-    <div className="flex flex-col justify-between text-xs gap-y-3 px-2 font-normal h-full">
-      <div className=" p-6">
-        <img src={sample} alt="" />
-      </div>
-
-      <div className="flex flex-wrap flex-col gap-x-3">
-        <p className="font-bold text-gray-700 flex-wrap">{data.title}</p>
-        <p className="text-gray-500">{data.author}</p>
-      </div>
-      <div className="">
-        <p className="text-black">{data.location}</p>
-        <div className="flex flex-col mini:flex-row justify-between lg:gap-8">
-          <p className="text-[#3BC3FF] ">{data.seller}</p>
-          <p className="text-black text-base ">{data.price}TL</p>
+    <Link state={data} to={`/${data.id}`}>
+      <div className="flex flex-col justify-between text-xs gap-y-3 px-2 font-normal h-full">
+        <div className=" p-6">
+          <img src={sample} alt="" />
         </div>
-      </div>
 
-      <Button
-        onClick={() => addToCart({ data })}
-        className="bg-white border border-black  "
-      >
-        Add To Cart
-      </Button>
-    </div>
+        <div className="flex flex-wrap flex-col gap-x-3">
+          <p className="font-bold text-gray-700 flex-wrap">{data.title}</p>
+          <p className="text-gray-500">{data.author}</p>
+        </div>
+        <div className="">
+          <p className="text-black">{data.location}</p>
+          <div className="flex flex-col mini:flex-row justify-between lg:gap-8">
+            <p className="text-[#3BC3FF] ">{data.seller}</p>
+            <p className="text-black text-base ">{data.price}TL</p>
+          </div>
+        </div>
+
+        <Button
+          onClick={() => addToCart({ data })}
+          className="bg-white border border-black  "
+        >
+          Add To Cart
+        </Button>
+      </div>
+    </Link>
   ) : (
     <Spin />
   );
--- src/common/view/booksView.js
+++ src/common/view/booksView.js
@@ -1,13 +1,16 @@
+import { Col, Row } from "antd";
 import BookCard from "./../components/card/book-card";
 const BooksView = ({ bookData, title }) => {
   return (
-    <div className="bg-white py-10 px-14" >
+    <div className="bg-white py-10 px-14">
       <p className="mb-8 text-lg font-bold">{title}</p>
-      <div className={" no-scrollbar flex flex-row flex-nowrap overflow-x-scroll gap-x-4 "}>
+      <Row className={" no-scrollbar flex-nowrap overflow-x-scroll gap-x-4 "}>
         {bookData.map((book, idx) => (
-          <BookCard book={book} />
+          <Col span={3}>
+            <BookCard book={book} />
+          </Col>
         ))}
-      </div>
+      </Row>
     </div>
   );
 };
